9cbdef377391d0830d1889e36d77ec876c40b892
stacktrace828 <stacktrace828@gmail.com>
2016-03-13 01:12:52 +0800

    bug fix

--- app/src/main/java/com/stacktrace/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/stacktrace/criminalintent/CrimePagerActivity.java
@@ -1,5 +1,7 @@
 package com.stacktrace.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -8,15 +10,28 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 
 import java.util.List;
+import java.util.UUID;
 
 public class CrimePagerActivity extends FragmentActivity {
+    private static final String EXTRA_CRIME_ID =
+            "com.stackhacks.criminalintent.crime_id";
+    
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+
+        UUID crimeId = (UUID) getIntent()
+                .getSerializableExtra(EXTRA_CRIME_ID);
 
         mViewPager = (ViewPager) findViewById(R.id.activity_crime_pager_view_pager);
         mCrimes = CrimeLab.get(this).getCrimes();
