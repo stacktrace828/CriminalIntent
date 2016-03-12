2b846bb5ebd0548c33e649ea2482256821c6309d
stacktrace828 <stacktrace828@gmail.com>
2016-03-12 22:43:51 +0800

    add recyclerview

---
+++ app/src/main/java/com/stacktrace/criminalintent/CrimeLab.java
@@ -0,0 +1,45 @@
+package com.stacktrace.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by stackhacks on 2016/3/12.
+ */
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
+
+    private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0); // Every other one
+            mCrimes.add(crime);
+        }
+    }
+
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
+    }
+}
